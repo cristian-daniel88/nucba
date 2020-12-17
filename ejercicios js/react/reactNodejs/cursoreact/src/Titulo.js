9dc7b7e866ea8fde1cb0df9e700964c77b735090
cristian daniel herrera <cristian.daniel.herrera@hotmail.com>
2020-12-17 22:06:38 +0000

    Nucba

---
+++ ejercicios js/react/reactNodejs/cursoreact/src/Titulo.js
@@ -0,0 +1,12 @@
+
+import './Titulo.css';
+
+function Titulo() {
+  return (
+    <div className="Titulo">
+        <h1>Holaaa</h1>
+    </div>
+  );
+}
+
+export default Titulo;
--- ejercicios/peso  en la luna/index.js
+++
@@ -1,9 +0,0 @@
-const peso = document.querySelector('#peso');
-const resultado = document.querySelector('#resultado');
-
-peso.addEventListener('input', callbackPeso);
-
-function callbackPeso (e) {
-  resultado.textContent = Number(e.target.value)/6;
-}
-
